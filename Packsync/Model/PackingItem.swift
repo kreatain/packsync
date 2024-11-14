d4129903c99236e9f8a4bdd069cec797a5689320
Xi Jia <105234432+xijia007@users.noreply.github.com>
2024-11-14 12:50:56 -0800

    Update PackingItem.swift

--- Packsync/Model/PackingItem.swift
+++ Packsync/Model/PackingItem.swift
@@ -7,19 +7,29 @@
 import Foundation
 
 struct PackingItem: Codable {
-    var id: String
-    var creatorEmail: String
-    var travelTitle: String
+    var id: String // Unique identifier for the packing item
+    var creatorId: String // ID of the user who created the item
+    var travelId: String // ID of the travel plan the item is associated with
     var name: String
     var isPacked: Bool
-    var itemNumber: String?
+    var isPackedBy: String?
+    var itemNumber: String // Quantity or count of the item
     
-    init(id: String = UUID().uuidString, creatorEmail: String, travelTitle: String, name: String, isPacked: Bool = false, itemNumber: String) {
+    init(
+        id: String = UUID().uuidString,
+        creatorId: String,
+        travelId: String,
+        name: String,
+        isPacked: Bool = false,
+        isPackedBy: String? = nil, 
+        itemNumber: String
+    ) {
         self.id = id
-        self.creatorEmail = creatorEmail
-        self.travelTitle = travelTitle
+        self.creatorId = creatorId
+        self.travelId = travelId
         self.name = name
         self.isPacked = isPacked
+        self.isPackedBy = isPackedBy
         self.itemNumber = itemNumber
     }
 }
